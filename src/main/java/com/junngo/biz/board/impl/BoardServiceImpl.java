8c9c8a0788440ca4db41fcd692db13cc20180520
MyeongJun Go <myeongjun.ko@gmail.com>
2020-11-24 15:00:12 +0900

    first commit

---
+++ src/main/java/com/junngo/biz/board/impl/BoardServiceImpl.java
@@ -0,0 +1,36 @@
+package com.junngo.biz.board.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.junngo.biz.board.BoardService;
+import com.junngo.biz.board.BoardVO;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardDAO boardDAO;
+
+	public void insertBoard(BoardVO vo) {
+		boardDAO.insertBoard(vo);
+	}
+
+	public void updateBoard(BoardVO vo) {
+		boardDAO.updateBoard(vo);
+	}
+
+	public void deleteBoard(BoardVO vo) {
+		boardDAO.deleteBoard(vo);
+	}
+
+	public BoardVO getBoard(BoardVO vo) {
+		return boardDAO.getBoard(vo);
+	}
+
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		return boardDAO.getBoardList(vo);
+	}
+}
