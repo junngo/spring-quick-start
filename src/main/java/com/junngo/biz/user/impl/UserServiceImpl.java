9b0e237ee163500ff70dcc23d10dee8c7e768a7c
MyeongJun Go <myeongjun.ko@gmail.com>
2020-11-24 15:46:43 +0900

    created user login logic

---
+++ src/main/java/com/junngo/biz/user/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.junngo.biz.user.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.junngo.biz.user.UserService;
+import com.junngo.biz.user.UserVO;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserDAO userDAO;
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public UserVO getUser(UserVO vo) {
+		return userDAO.getUser(vo);
+	}
+}
