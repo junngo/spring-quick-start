c786e648aa52c48fe58880983f1ea14a2aef5a5f
MyeongJun Go <myeongjun.ko@gmail.com>
2020-11-28 11:22:08 +0900

    using annotaion for controller

--- src/main/java/com/junngo/view/user/LoginController.java
+++ src/main/java/com/junngo/view/user/LoginController.java
@@ -1,41 +1,20 @@
 package com.junngo.view.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.junngo.biz.user.UserVO;
 import com.junngo.biz.user.impl.UserDAO;
 
+@Controller
+public class LoginController {
 
-public class LoginController implements Controller {
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("로그인 처리");
-		
-		// 1. 사용자 입력 정보 추출
-		String id = request.getParameter("id");
-		String password = request.getParameter("password");
-
-		// 2. DB 연동 처리
-		UserVO vo = new UserVO();
-		vo.setId(id);
-		vo.setPassword(password);
-
-		UserDAO userDAO = new UserDAO();
-		UserVO user = userDAO.getUser(vo);
-
-		ModelAndView mav = new ModelAndView();
-		// 3. 화면 네비게이션
-		if (user != null) {
-			mav.setViewName("redirect:getBoardList.do");
-		} else {
-			mav.setViewName("redirect:login.jsp");
-		}
-		return mav;
+	@RequestMapping("/login.do")
+	public String login(UserVO vo, UserDAO userDAO) {
+		if (userDAO.getUser(vo) != null)
+			return "getBoardList.do";
+		else
+			return "login.jsp";
 	}
 
 }
